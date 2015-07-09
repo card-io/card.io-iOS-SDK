04ad51a3b8a2a79870332d6fe861e63dc393a4d9
Kyle McAlpine <k@kylejm.io>
2015-07-09 14:48:18 +0100

    Xcode project added for Carthage

---
+++ CardIOTests/CardIOTests.m
@@ -0,0 +1,40 @@
+//
+//  CardIOTests.m
+//  CardIOTests
+//
+//  Created by Kyle McAlpine on 09/07/2015.
+//  Copyright (c) 2015 Kyle McAlpine. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface CardIOTests : XCTestCase
+
+@end
+
+@implementation CardIOTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
